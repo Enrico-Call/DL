487900ed21520c60e714c1c2cd9b0ec478823ddf
enrico <calleris.enrico@gmail.com>
2021-11-22 21:57:28 +0100

    updates

---
+++ Project 2/basicNetwork.py
@@ -0,0 +1,17 @@
+from vugrad import vugrad as vg
+import numpy as np
+
+# Create two Tensor nodes with
+a = vg.TensorNode(np.random.randn(2, 2))
+b = vg.TensorNode(np.random.randn(2, 2))
+
+# Create another Tensor node with a sum operation
+
+c = a + b
+
+# Check the properties of c
+
+print(c.value)
+print(c.source)
+print(c.source.inputs[0].value)
+print(a.grad)
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Deep Learning
 
 Deep Learning Applications for the Course Deep Leaning (2021) at Vrije Universiteit Amsterdam.
+
+- basicNetwork.py, contains the code for creating a basic computational graph with two tensors and a sum operation
